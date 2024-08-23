49f3bcca98f0f4e654f69124853fee97302a16b4
Zated21 <zated21@gmail.com>
2024-08-23 18:12:05 +0600

    work process section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: { 
-  },
-    extend: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
   },
   plugins: [],
 }
